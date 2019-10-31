9684382595a8907ff2a070ba9925b1522392d19a
Sean Eriksen <eriksensean@yahoo.com>
2019-10-31 12:54:44 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+    def initialize(name, breed)
+        @name = name
+        @breed = breed
+    end
+
+    def name
+        @name
+    end
+    
+    def breed
+      @breed = breed
+      if breed = ()
+        puts "Mutt"
+      end
+    end
+  end
